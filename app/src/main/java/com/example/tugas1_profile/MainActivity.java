4ec6e125bb44ebc1e40489365df83991f7198f0e
edhopaksi <edhopaksisaputra@gmail.com>
2019-12-28 16:12:56 +0700

    tambah menu bahasa

--- app/src/main/java/com/example/tugas1_profile/MainActivity.java
+++ app/src/main/java/com/example/tugas1_profile/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.tugas1_profile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageView ivsetting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+        ivsetting = findViewById(R.id.ivsetting);
+        ivsetting.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        Intent setting = new Intent(MainActivity.this,app_setting.class);
+        startActivity(setting);
     }
 }
